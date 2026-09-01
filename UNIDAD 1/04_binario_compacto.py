831ea7d326ad2148b2a3cbea5b346a9280f5425d
David Pichardo <davpapu08@gmail.com>
2026-09-01 10:49:05 -0600

    U

--- UNIDAD 1/04_binario_compacto.py
+++ UNIDAD 1/04_binario_compacto.py
@@ -1,4 +1,4 @@
-numero = 9
-binario = ""
-while numero > 0: binario, numero = str(numero % 2) + binario, numero // 2
+numero = 9                                           #aqui estamos indicando el numero
+binario = ""                                         #aqui estamos dando la señal de lo que tiene que aparecer como binario
+while numero > 0: binario, numero = str(numero % 2) + binario, numero // 2   #aqui estamos realizando la conversión a binario
 print(binario)
